d36fd351b5df3fe429b23c4c2e982b3124576cba
Chris Crook <ccrook@linz.govt.nz>
2016-06-03 09:51:49 +1200

    Fixing land extents buffering to avoid absorbing entire CPU etc

--- python/buffer_land_extents.py
+++ python/buffer_land_extents.py
@@ -2,8 +2,9 @@
 
 from osgeo import ogr
 from shapely import wkt, wkb
-from shapely.ops import unary_union, transform
 from shapely.geometry import MultiPolygon, Polygon
+from shapely.ops import cascaded_union
+from shapely import affinity
 import math
 import sys
 
@@ -16,7 +17,28 @@ import sys
 land_area_buffer=20000
 land_area_tolerance=5000
 
-def load_land_areas( polygon_shapefile, extents_wktfile, buffer=land_area_buffer, tolerance=land_area_tolerance ):
+def buffered_polygon( polygon, buffer, simplify=0.0 ):
+    '''
+    Buffer a polygon by an extents in metres.  Approximately scales to metres, applies buffer,
+    and scales back. Returns a multipolygon
+    '''
+
+    if buffer > 0:
+        minx,miny,maxx,maxy=polygon.bounds
+        midx=(minx+maxx)/2.0
+        midy=(miny+maxy)/2.0
+        origin=[midx,midy,0.0]
+        scale_factor=(1.0e-5/math.cos(math.radians(midy)),1.0e-5)
+        polygon=affinity.scale(polygon,xfact=1.0/scale_factor[0],yfact=1.0/scale_factor[1],origin=origin)
+        # Simplifying first to speed up process
+        polygon=polygon.simplify(buffer/10.0)
+        polygon=polygon.buffer(buffer)
+        if simplify > 0:
+            polygon=polygon.simplify(simplify)
+        polygon=affinity.scale(polygon,xfact=scale_factor[0],yfact=scale_factor[1],origin=origin)
+    return polygon
+
+def create_land_areas( polygon_shapefile, extents_wktfile, buffer=land_area_buffer, tolerance=land_area_tolerance, min_points=0, verbose=False ):
     areas=[]
     driver=ogr.GetDriverByName('ESRI Shapefile')
     print("Loading land area definition from "+polygon_shapefile)
@@ -24,29 +46,42 @@ def load_land_areas( polygon_shapefile, extents_wktfile, buffer=land_area_buffer
     if datasource is None:
         raise RuntimeError('Cannot open land areas file '+polygon_file)
     layer=datasource.GetLayer()
+    npoints=0
+    nskip=0
+    areas=[]
     for feature in layer:
         mp=wkb.loads(feature.GetGeometryRef().ExportToWkb())
-        if type(mp) != MultiPolygon:
+        if type(mp) == Polygon:
             mp=[mp]
         for p in mp:
-            print("Polygon: {0} points".format(len(list(p.exterior.coords))))
+            npoints1=len(p.exterior.coords)
+            if min_points and len(p.exterior.coords) < min_points:
+                nskip += 1
+                continue
             p=Polygon(p.exterior)
+            p=buffered_polygon(p,buffer,tolerance)
+            npoints2=len(p.exterior.coords)
+            if type(p) == Polygon:
+                p=[p]
+            areas.extend(p)
+            npoints += npoints2
+            if verbose:
+                print("Polygon: {0} points reduced to {1} points".format(npoints1,npoints2))
 
-            minx,miny,maxx,maxy=p.bounds
-            midx=(minx+maxx)/2.0
-            midy=(miny+maxy)/2.0
-            yscale=100000
-            xscale=100000*math.cos(math.radians(midy))
-            p=transform(lambda x,y,z=None: ((x-midx)*xscale,(y-midy)*yscale,0.0),p)
-            p=p.buffer(buffer,3).simplify(tolerance)
-            p=transform(lambda x,y,z=None: (x/xscale+midx,y/yscale+midy,0.0),p)
-            areas.append(p)
+    if verbose:
+        print("Skipped {0} polygons < {1} points".format(nskip,min_points))
     if areas:
-        land_areas=unary_union(areas)
+        if verbose:
+            print("Forming union of areas - total of {0} points in {1} polygons"
+              .format(npoints,len(areas)))
+        areas=MultiPolygon(areas)
+        areas=areas.buffer(0)
         try:
+            if verbose:
+                print("Writing wkt file {0}".format(extents_wktfile))
             from shapely.wkt import dumps
             with open(extents_wktfile,"w") as laf:
-                laf.write(dumps(land_areas))
+                laf.write(dumps(areas))
         except:
             pass
 
@@ -58,11 +93,14 @@ if __name__=="__main__":
     parser.add_argument("output_wktfile",help="Output buffered island WKT file")
     parser.add_argument("-b","--buffer",type=float,default=land_area_buffer,help="Buffer applied to island polygons")
     parser.add_argument("-t","--tolerance",type=float,default=land_area_tolerance,help="Tolerance for shape simplification")
+    parser.add_argument("-p","--min-points",type=int,default=0,help="Tolerance for shape simplification")
     parser.add_argument("-f","--force",action="store_true",help="Force building even if current version already built")
+    parser.add_argument("-v","--verbose",action="store_true",help="Print output during calculations")
 
     args=parser.parse_args()
     sfile=args.nzpoly_shapefile
     ofile=args.output_wktfile
+    verbose=args.verbose
 
     if not os.path.exists(sfile):
         print "NZ polygon shape file "+sfile+" does not exist - aborting!"
@@ -77,7 +115,9 @@ if __name__=="__main__":
             sys.exit()
         except Exception as ex:
             print("Cannot read existing polygon WKT in "+ofile)
-    print("Building coastline polygon extents")
-    print("Buffering by {0} degrees".format(args.buffer))
-    print("Simplification tolerance {0} degrees".format(args.tolerance))
-    load_land_areas(sfile,ofile,buffer=args.buffer,tolerance=args.tolerance)
+    if verbose:
+        print("Building coastline polygon extents")
+        print("Buffering by {0} degrees".format(args.buffer))
+        print("Simplification tolerance {0} degrees".format(args.tolerance))
+    create_land_areas(sfile,ofile,buffer=args.buffer,tolerance=args.tolerance,
+                      min_points=args.min_points,verbose=verbose)
